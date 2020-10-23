dc0b3e89d36e8ec43226a25d3eab2d2831c17b05
justengel <jtengel08@gmail.com>
2020-10-23 12:51:44 -0400

    Updated to version 0.3.0

--- pybk8500/__meta__.py
+++ pybk8500/__meta__.py
@@ -1,5 +1,5 @@
 name = 'pybk8500'
-version = '0.2.1'
+version = '0.3.0'
 description = 'BK-8500-Electronic-Load python library'
 url = 'https://github.com/justengel/pybk8500'
 author = 'Justin Engel'
