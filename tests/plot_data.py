6a9748033c783a0081ec391359067dfb9dc83760
justengel <jtengel08@gmail.com>
2020-11-19 08:03:39 -0500

    Updated plot_data.py test to show how you can add a grid to the plots.

--- tests/plot_data.py
+++ tests/plot_data.py
@@ -1,4 +1,14 @@
 from pybk8500 import plot_csv_file, plot_csv_files
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
-    plot_csv_files('./CC_1.csv', './CC_2.csv', inc_time=True, split=False)
+    figures = plot_csv_files('./CC_1.csv', './CC_2.csv', inc_time=True, split=False, show=False)
+
+    # Change figures
+    # for fig in figures:
+    #     fig.gca().grid()
+
+    # Use plt
+    plt.grid()
+    plt.show()
